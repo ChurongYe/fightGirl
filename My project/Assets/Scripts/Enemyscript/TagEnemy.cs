b7b1e1edc84ba1e6e903441d4bd0687782e93d74
ChurongYe <yechurong0916@163.com>
2025-03-22 22:39:18 +0800

    level2

--- My project/Assets/Scripts/Enemyscript/TagEnemy.cs
+++ My project/Assets/Scripts/Enemyscript/TagEnemy.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TagEnemy : MonoBehaviour
 {
     public float attackInterval = 4f;
-    public GameObject Hiterea;
+    public GameObject[] Hiterea;
     public void Start()
     {
-        Hiterea.SetActive(false);
-        InvokeRepeating(nameof(StartFiring), 0f, attackInterval);
+        UpdateEnemyList();
+        foreach (GameObject hiterea in Hiterea)
+        {
+            hiterea.SetActive(false);
+            InvokeRepeating(nameof(StartFiring), 0f, attackInterval);
+        }
     }
     void StartFiring()
     {
         StartCoroutine(FireAttack());
     }
+    void UpdateEnemyList()
+    {
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+
+        Hiterea = allObjects.Where(obj => obj.name == "TagEnemy").ToArray();
+    }
 
     IEnumerator FireAttack()
     {
-        Hiterea.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        Hiterea.SetActive(false);
+        foreach (GameObject hiterea in Hiterea)
+        {
+            hiterea.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            hiterea.SetActive(false);
+        }
 
     }
 }
